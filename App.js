52b1acb6f4ab765061f9feb57485fb139f656859
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2022-12-10 16:58:13 +0700

    implement navigation

--- App.js
+++ App.js
@@ -2,42 +2,65 @@ import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
 
 import ManageExpense from "./screens/ManageExpense";
 import RecentExpenses from "./screens/RecentExpenses";
 import AllExpenses from "./screens/AllExpenses";
 import { GlobalStyles } from "./constants/styles";
+import IconButton from "./components/UI/IconButton";
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
 function ExpensesOverview() {
   return (
-    <BottomTabs.Navigator screenOptions={{
-      headerStyle: {
-        backgroundColor: GlobalStyles.colors.primary500,
-      },
-      headerTintColor: 'white',
-      tabBarStyle: {
-        backgroundColor: GlobalStyles.colors.primary500,
-      },
-      tabBarActiveTintColor: GlobalStyles.colors.accent500
-    }}>
-      <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses} options={{
-        title: 'Recent Expenses',
-        tabBarLabel: 'Recent',
-        tabBarIcon: ({ color, size }) => {
-          return <Ionicons name="hourglass" size={size} color={color} />;
-        }
-      }}/>
-      <BottomTabs.Screen name="AllExpenses" component={AllExpenses} options={{
-        title: 'All Expenses',
-        tabBarLabel: 'All',
-        tabBarIcon: ({ color, size }) => {
-          return <Ionicons name="calendar" size={size} color={color} />;
-        }
-      }}/>
+    <BottomTabs.Navigator
+      screenOptions={({ navigation }) => ({
+        headerStyle: {
+          backgroundColor: GlobalStyles.colors.primary500,
+        },
+        headerTintColor: "white",
+        tabBarStyle: {
+          backgroundColor: GlobalStyles.colors.primary500,
+        },
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
+        headerRight: ({ tintColor }) => {
+          return (
+            <IconButton
+              icon="add"
+              size={24}
+              color={tintColor}
+              onPress={() => {
+                navigation.navigate("ManageExpense");
+              }}
+            />
+          );
+        },
+      })}
+    >
+      <BottomTabs.Screen
+        name="RecentExpenses"
+        component={RecentExpenses}
+        options={{
+          title: "Recent Expenses",
+          tabBarLabel: "Recent",
+          tabBarIcon: ({ color, size }) => {
+            return <Ionicons name="hourglass" size={size} color={color} />;
+          },
+        }}
+      />
+      <BottomTabs.Screen
+        name="AllExpenses"
+        component={AllExpenses}
+        options={{
+          title: "All Expenses",
+          tabBarLabel: "All",
+          tabBarIcon: ({ color, size }) => {
+            return <Ionicons name="calendar" size={size} color={color} />;
+          },
+        }}
+      />
     </BottomTabs.Navigator>
   );
 }
--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,4 +1,5 @@
-import { FlatList, Text, View } from "react-native"
+import { FlatList, Text, View, StyleSheet } from "react-native"
+import { GlobalStyles } from "../../constants/styles"
 import ExpensesList from "./ExpensesList"
 import ExpensesSummary from "./ExpensesSummary"
 
@@ -26,12 +27,36 @@ const DUMMY_EXPENSES = [
         description: 'anu',
         amount: 31,
         date: new Date('2022-02-04')
+    },
+    {
+        id: 'e5',
+        description: 'New TV',
+        amount: 799.49,
+        date: new Date('2021-12-19')
+    },
+    {
+        id: 'e6',
+        description: 'A new desk (wooden)',
+        amount: 450,
+        date: new Date('2022-01-01')
+    },
+    {
+        id: 'e7',
+        description: 'Car Insurance',
+        amount: 250,
+        date: new Date('2022-02-01')
+    },
+    {
+        id: 'e8',
+        description: 'anu',
+        amount: 31,
+        date: new Date('2022-02-04')
     }
 ]
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
     return (
-        <View>
+        <View style={styles.container}>
             <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod}/>
             <ExpensesList expenses={DUMMY_EXPENSES} />
         </View>
@@ -39,3 +64,13 @@ function ExpensesOutput({ expenses, expensesPeriod }) {
 }
 
 export default ExpensesOutput
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 16,
+        paddingTop: 24,
+        paddingBottom: 0,
+        backgroundColor: GlobalStyles.colors.primary700,
+    }
+})
